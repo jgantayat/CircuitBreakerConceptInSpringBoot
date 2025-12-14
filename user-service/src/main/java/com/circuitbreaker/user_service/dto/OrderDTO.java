47df626d418f2d22c9f4b6d7f001a022568f9576
jgantayat <gantayatjanmajaya@gmail.com>
2025-12-14 19:51:24 +0530

    Adding sample code for the skeleton implementation for the Circuit breaker concept in microservice communication

---
+++ user-service/src/main/java/com/circuitbreaker/user_service/dto/OrderDTO.java
@@ -0,0 +1,28 @@
+package com.circuitbreaker.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+    private int id;
+    private String name;
+    private String category;
+    private String color;
+    private double price;
+
+//    public OrderDTO() {
+//    }
+//
+//    public OrderDTO(int id, String name, String category, String color, double price) {
+//        this.id = id;
+//        this.name = name;
+//        this.category = category;
+//        this.color = color;
+//        this.price = price;
+//    }
+}
